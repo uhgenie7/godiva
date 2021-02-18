4acafd0866cd60159fe296d5324f518c412dd6dc
hyejin <dzzd777@naver.com>
2021-02-18 17:07:41 +0900

    추가: 슬라이드 기능. swiper 라이브러리 사용

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper('.swiper-container', {
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination'
+      },
+      keyboard: true,
+    });
