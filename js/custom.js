1253fd1b2f502ce566163445e04ddb6315c20531
hyejin <dzzd777@naver.com>
2021-02-23 18:40:07 +0900

    추가: 내비게이션 클릭에 따른 효과 추가 show, none 기능, 많은 이벤트 리스너로 인해 이벤트 위임 필요 요망

--- js/custom.js
+++ js/custom.js
@@ -1,97 +1,97 @@
-depth2 = document.querySelectorAll(".depth2");
-depth1List = document.querySelectorAll(".depth1_list");
-depth2Info = document.querySelectorAll(".depth2_info");
-depth2Listbox = document.querySelectorAll(".depth2_listbox");
+// depth2 = document.querySelectorAll(".depth2");
+// depth1List = document.querySelectorAll(".depth1_list");
+// depth2Info = document.querySelectorAll(".depth2_info");
+// depth2Listbox = document.querySelectorAll(".depth2_listbox");
 
-depth1List[0].addEventListener("mouseover", function () {
-  depth2[0].classList.add("on");
-  depth2Info[0].classList.add("on");
-  depth2Listbox[0].classList.add("on");
+// depth1List[0].addEventListener("mouseover", function () {
+//   depth2[0].classList.add("on");
+//   depth2Info[0].classList.add("on");
+//   depth2Listbox[0].classList.add("on");
 
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
-depth1List[1].addEventListener("mouseover", function () {
-  depth2[1].classList.add("on");
-  depth2Info[1].classList.add("on");
-  depth2Listbox[1].classList.add("on");
+// depth1List[1].addEventListener("mouseover", function () {
+//   depth2[1].classList.add("on");
+//   depth2Info[1].classList.add("on");
+//   depth2Listbox[1].classList.add("on");
 
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
-
-
-depth1List[2].addEventListener("mouseover", function () {
-  depth2[2].classList.add("on");
-  depth2Info[2].classList.add("on");
-  depth2Listbox[2].classList.add("on");
-
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
-
-depth1List[3].addEventListener("mouseover", function () {
-  depth2[3].classList.add("on");
-  depth2Info[3].classList.add("on");
-  depth2Listbox[3].classList.add("on");
-
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-});
-
-depth2[0].addEventListener("mouseleave", function () {
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-})
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
 
-depth2[1].addEventListener("mouseleave", function () {
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-});
+// depth1List[2].addEventListener("mouseover", function () {
+//   depth2[2].classList.add("on");
+//   depth2Info[2].classList.add("on");
+//   depth2Listbox[2].classList.add("on");
 
-depth2[2].addEventListener("mouseleave", function () {
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-});
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
-depth2[3].addEventListener("mouseleave", function () {
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
+// depth1List[3].addEventListener("mouseover", function () {
+//   depth2[3].classList.add("on");
+//   depth2Info[3].classList.add("on");
+//   depth2Listbox[3].classList.add("on");
+
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+// });
+
+// depth2[0].addEventListener("mouseleave", function () {
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+// })
+
+
+// depth2[1].addEventListener("mouseleave", function () {
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+// });
+
+// depth2[2].addEventListener("mouseleave", function () {
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+// });
+
+// depth2[3].addEventListener("mouseleave", function () {
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
 
 // scroll event dom
@@ -102,6 +102,7 @@ const headerHeight = headerTop.getBoundingClientRect().height;
 const sliderHeight = slider.getBoundingClientRect().height;
 const depth1 = document.querySelector(".depth1");
 const contentTop = document.querySelector(".content_top");
+const faBars = document.querySelector(".fa-bars");
 
 
 // gnb & contentTop display show or none 
@@ -109,13 +110,15 @@ document.addEventListener('scroll', scrollEvent);
 
 function scrollEvent() {
     if (window.scrollY > headerHeight) {
-      headerTop.style.display = "none"
-      headerInfo2[0].classList.add("show");
-      headerInfo2[1].classList.add("show");
+      // headerTop.style.display = "none"
+      // headerInfo2[0].classList.add("show");
+      // headerInfo2[1].classList.add("show");
+      // faBars.classList.add("show");
   } else {
-      headerTop.style.display = "flex";
-      headerInfo2[0].classList.remove("show");
-      headerInfo2[1].classList.remove("show");  
+      // headerTop.style.display = "flex";
+      // headerInfo2[0].classList.remove("show");
+      // headerInfo2[1].classList.remove("show");  
+      // faBars.classList.remove("show");
   }
 
  if (window.scrollY > sliderHeight / 2) {
@@ -131,7 +134,9 @@ scrollEvent();
 // search icon click event
 const searchIcon = document.querySelectorAll(".fa-search");
 const headerBottom = document.querySelector(".header_bottom");
-const faTimes = document.querySelector(".fa-times");
+const faTimes = document.querySelectorAll(".fa-times");
+const headerNav = document.querySelector(".header_nav");
+
 searchIcon[0].addEventListener("click", searchShow);
 searchIcon[1].addEventListener("click", searchShow);
 
@@ -139,8 +144,23 @@ function searchShow() {
   headerBottom.classList.add("show");
 }
 
+// nav bar click event 
+faBars.addEventListener("click", () => {
+  faTimes[0].classList.add("show");
+  faBars.classList.add("show");
+  headerNav.classList.add("show");
+});
+
+faTimes[0].addEventListener("click", () => {
+  faTimes[0].classList.remove("show");
+  faBars.classList.remove("show");
+  headerNav.classList.remove("show");
+
+});
+
+
 // search input close
-faTimes.addEventListener("click", () => {
+faTimes[1].addEventListener("click", () => {
 headerBottom.classList.remove("show");
 });
 
