e407b408449f2ec47b8985c50b0222301ec337ef
hyejin <dzzd777@naver.com>
2021-02-24 15:49:32 +0900

    추가: 스와이퍼 기능 추가 자동 넘김 기능, 페이지 내비게이션 클릭 기능 추가 스와이퍼 next, prev 이미지 추가

--- js/swiper.js
+++ js/swiper.js
@@ -1,11 +1,19 @@
 let swiper = new Swiper('.swiper-container', {
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination'
-      },
-      keyboard: true,
-    });
+  slidesPerView: 1,
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable : true
+  },
+  keyboard: true,
+  touchRatio: 0,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpointsInverse: true,
+});
