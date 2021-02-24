171589032e426301ddb9a8abba29ee5869974d42
hyejin <dzzd777@naver.com>
2021-02-24 18:14:35 +0900

    리팩토링 및 버그 수정 리팩토링: 내비게이션 mouseover, leave 리팩토링, 반복문을 사용함 수정: document에 걸어둔 click 이벤트가 평소에도 작동하여 배경을 눌렀을 때 토글이 생기는 현상을 조건문으로 수정

--- js/custom.js
+++ js/custom.js
@@ -3,95 +3,128 @@ depth1List = document.querySelectorAll(".depth1_list");
 depth2Info = document.querySelectorAll(".depth2_info");
 depth2Listbox = document.querySelectorAll(".depth2_listbox");
 
-depth1List[0].addEventListener("mouseover", function () {
-  depth2[0].classList.add("on");
-  depth2Info[0].classList.add("on");
-  depth2Listbox[0].classList.add("on");
+for (let i = 0; i < depth1List.length; i++) {
+  depth1List[i].addEventListener("mouseover", () => {
+    depth2[i].classList.add("on");
+    depth2Info[i].classList.add("on");
+    depth2Listbox[i].classList.add("on");
+  });
+}
 
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
+for (let i = 0; i < depth1List.length; i++) {
+  depth1List[i].addEventListener("mouseleave", () => {
+    depth2[i].classList.remove("on");
+    depth2Info[i].classList.remove("on");
+    depth2Listbox[i].classList.remove("on");
+  });
+}
 
-depth1List[1].addEventListener("mouseover", function () {
-  depth2[1].classList.add("on");
-  depth2Info[1].classList.add("on");
-  depth2Listbox[1].classList.add("on");
-
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
+for (let i = 0; i < depth2.length; i++) {
+  depth2[i].addEventListener("mouseleave", () => {
+    depth2[i].classList.remove("on");
+    depth2Info[i].classList.remove("on");
+    depth2Listbox[i].classList.remove("on");  
+  });
+}
 
 
-depth1List[2].addEventListener("mouseover", function () {
-  depth2[2].classList.add("on");
-  depth2Info[2].classList.add("on");
-  depth2Listbox[2].classList.add("on");
+// depth1List[0].addEventListener("mouseover", function () {
+//   depth2[0].classList.add("on");
+//   depth2Info[0].classList.add("on");
+//   depth2Listbox[0].classList.add("on");
 
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
-depth1List[3].addEventListener("mouseover", function () {
-  depth2[3].classList.add("on");
-  depth2Info[3].classList.add("on");
-  depth2Listbox[3].classList.add("on");
+// depth1List[1].addEventListener("mouseover", function () {
+//   depth2[1].classList.add("on");
+//   depth2Info[1].classList.add("on");
+//   depth2Listbox[1].classList.add("on");
 
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-});
-
-depth2[0].addEventListener("mouseleave", function () {
-  depth2[0].classList.remove("on");
-  depth2Info[0].classList.remove("on");
-  depth2Listbox[0].classList.remove("on");
-})
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
 
-depth2[1].addEventListener("mouseleave", function () {
-  depth2[1].classList.remove("on");
-  depth2Info[1].classList.remove("on");
-  depth2Listbox[1].classList.remove("on");
-});
+// depth1List[2].addEventListener("mouseover", function () {
+//   depth2[2].classList.add("on");
+//   depth2Info[2].classList.add("on");
+//   depth2Listbox[2].classList.add("on");
 
-depth2[2].addEventListener("mouseleave", function () {
-  depth2[2].classList.remove("on");
-  depth2Info[2].classList.remove("on");
-  depth2Listbox[2].classList.remove("on");
-});
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
-depth2[3].addEventListener("mouseleave", function () {
-  depth2[3].classList.remove("on");
-  depth2Info[3].classList.remove("on");
-  depth2Listbox[3].classList.remove("on");
-});
+// depth1List[3].addEventListener("mouseover", function () {
+//   depth2[3].classList.add("on");
+//   depth2Info[3].classList.add("on");
+//   depth2Listbox[3].classList.add("on");
+
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+// });
+
+
+
+// for (let i = 0; i < depth1List.length; i++) {
+//   depth2[i].addEventListener("mouseleave", removeOn);
+// }
+
+
+
+// depth2[0].addEventListener("mouseleave", function () {
+//   depth2[0].classList.remove("on");
+//   depth2Info[0].classList.remove("on");
+//   depth2Listbox[0].classList.remove("on");
+// })
+
+
+// depth2[1].addEventListener("mouseleave", function () {
+//   depth2[1].classList.remove("on");
+//   depth2Info[1].classList.remove("on");
+//   depth2Listbox[1].classList.remove("on");
+// });
+
+// depth2[2].addEventListener("mouseleave", function () {
+//   depth2[2].classList.remove("on");
+//   depth2Info[2].classList.remove("on");
+//   depth2Listbox[2].classList.remove("on");
+// });
+
+// depth2[3].addEventListener("mouseleave", function () {
+//   depth2[3].classList.remove("on");
+//   depth2Info[3].classList.remove("on");
+//   depth2Listbox[3].classList.remove("on");
+// });
 
 
 // scroll event dom
@@ -104,10 +137,9 @@ const faBars = document.querySelector(".fa-bars");
 
 
 // gnb & contentTop display show or none 
-document.addEventListener('scroll', scrollEvent);
 
 function scrollEvent() {
- if (window.scrollY > sliderHeight / 2) {
+  if (window.scrollY > sliderHeight / 2) {
     contentTop.classList.add("show");
   } else {
     contentTop.classList.remove("show")
@@ -116,6 +148,7 @@ function scrollEvent() {
 
 scrollEvent();
 
+document.addEventListener('scroll', scrollEvent);
 
 // search icon click event
 const searchIcon = document.querySelector(".fa-search");
@@ -123,21 +156,20 @@ const headerBottom = document.querySelector(".header_bottom");
 const faTimes = document.querySelectorAll(".fa-times");
 const headerNav = document.querySelector(".header_nav");
 
-searchIcon.addEventListener("click", searchShow);
+searchIcon.addEventListener("click", () => {
+  headerBottom.classList.add("show");
+  faTimes[1].style.display = "block";
+  searchIcon.style.display = "none";
+});
+
 faTimes[1].addEventListener("click", () => {
   headerBottom.classList.remove("show");
   faTimes[1].style.display = "none";
   searchIcon.style.display = "block";
 });
 
-function searchShow() {
-  headerBottom.classList.add("show");
-  faTimes[1].style.display = "block";
-  searchIcon.style.display = "none";
-}
 
-
-// nav bar click event 
+// navi bar click event 
 faBars.addEventListener("click", (e) => {
   e.stopPropagation();
   faTimes[0].style.display="block";
@@ -151,10 +183,15 @@ faTimes[0].addEventListener("click", () => {
   headerNav.classList.remove("show");
 });
 
-document.addEventListener("click", function () {
+document.addEventListener("click", () => {
+  let width = window.innerWidth;
+  if (width < 910) {   
     faBars.style.display="block";
     faTimes[0].style.display="none";
     headerNav.classList.remove("show");
+  } else {
+    return false;
+  }
 });
 
 // content_top click event
@@ -165,12 +202,11 @@ contentTop.addEventListener("click", () => {
     left: 0,
     top: wrap.offsetTop
   });
-})
-
+});
 
 // resize event
 function handleResize() {
-  const width = window.innerWidth;
+  let width = window.innerWidth;
   if (width > 910) {
     headerNav.classList.remove("show");
     faBars.style.display="none";
